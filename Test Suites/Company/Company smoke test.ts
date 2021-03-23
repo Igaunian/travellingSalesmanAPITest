7c9bdcdff6bbd74b5633620560e723b61be1024b
Igaunian <igaunian@gmail.com>
2021-03-23 21:01:22 +0100

    start (positive) smoke test for company

---
+++ Test Suites/Company/Company smoke test.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Company smoke test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6a88b70e-6539-4697-8879-7c279b6cf531</testSuiteGuid>
+   <testCaseLink>
+      <guid>1be912d5-8d0c-456a-926e-e591372a5329</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Company/registrate company</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7f59c55a-4fc3-42c2-b2ab-906955770a23</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Company/login company</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9d9665a2-330e-4a7d-9f0e-620090f7d64d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Company/get company</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
